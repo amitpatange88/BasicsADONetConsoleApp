19de537d61e4bc60207d1ca6e89d031e6645bd4f
Amit Patange <amitpatange88@gmail.com>
2018-08-22 17:54:54 +0530

    adding project files first time for ado.net demo app

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Configuration;
+
+namespace BasicsADONetConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("select COUNT(*) as TotalRows from EMPLOYEE", conn);
+                conn.Open();
+                int rows = (int)cmd.ExecuteScalar();
+            }
+        }
+    }
+}
